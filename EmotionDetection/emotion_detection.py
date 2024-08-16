e8ef1c89ff01a475eb5ef282e352b31b0ec2c52e
CristhianRam <cristhianramc@gmail.com>
2024-08-16 11:54:12 -0400

    Added errror handling in emotion_detection.py

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -9,15 +9,25 @@ def emotion_detector(text_to_analyse):
     response = requests.post(url, json = myobj, headers=header)
     
     formatted_response = json.loads(response.text)
-    formatted_response = formatted_response['emotionPredictions'][0]
-    formatted_response = formatted_response['emotion']
-    
-    dominant_score = -1
-    dominant_name = ""
-    for emotion in formatted_response.keys():
-        if formatted_response[emotion]>dominant_score:
-            dominant_name = emotion
-            dominant_score = formatted_response[emotion]
-    formatted_response["dominant_emotion"]=dominant_name
+
+    if response.status_code == 200:
+        formatted_response = formatted_response['emotionPredictions'][0]['emotion']
+        
+        dominant_score = -1
+        dominant_name = ""
+        for emotion in formatted_response.keys():
+            if formatted_response[emotion]>dominant_score:
+                dominant_name = emotion
+                dominant_score = formatted_response[emotion]
+        formatted_response["dominant_emotion"]=dominant_name
+
+    elif response.status_code == 400:
+        formatted_response = {
+                'anger': None,
+                'disgust': None, 
+                'fear': None, 
+                'joy': None, 
+                'sadness': None, 
+                'dominant_emotion': None}
   
     return (formatted_response)
