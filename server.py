f5d5434a48b5bf381d51857b02c0f2f706b79f06
CristhianRam <cristhianramc@gmail.com>
2024-08-16 12:18:25 -0400

    Modified server.py to pass the static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+Emotion Detection Web Application integrated with 
+Embeddable Watson AI libraries using Flask
+"""
+
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,25 +10,27 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def emotion_detect():
+    """Function to analyse the user text entry and return the results."""   
     text_to_analyze = request.args.get('textToAnalyze')
     response = emotion_detector(text_to_analyze)
 
-    if response["dominant_emotion"] == None:
+    if response["dominant_emotion"] is None:
         return "Invalid text! Please try again!."
 
     ans = "For the given statement, the system response is"
     for key in response.keys():
         if key=="dominant_emotion":
             continue
-        ans+=" \'{}\' : {},".format(key, response[key])
+        ans+=f" \'{key}\' : {response[key]},"
     lst = list(ans)
     lst[-1]="."
     ans = "".join(lst)
-    ans+=" The dominant emotion is {}.".format(response["dominant_emotion"])
+    ans+= f" The dominant emotion is {response['dominant_emotion']}."
     return ans
 
 @app.route("/")
 def render_index_page():
+    """Function to render the html file."""
     return render_template('index.html')
 
 if __name__ == "__main__":
